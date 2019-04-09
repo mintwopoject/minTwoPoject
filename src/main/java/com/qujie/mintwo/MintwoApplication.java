1e30f4dcf20c1149067b3e61eecc87341c8aafee
weiqinglin <weiqinglin>
2019-04-09 15:00:09 +0800

    222

--- src/main/java/com/qujie/mintwo/MintwoApplication.java
+++ src/main/java/com/qujie/mintwo/MintwoApplication.java
@@ -4,9 +4,12 @@ package com.qujie.mintwo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @SpringBootApplication
 @MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
@@ -22,6 +25,8 @@ public class MintwoApplication extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
         super.addResourceHandlers(registry);
     }
+
+
 
 
 }
--- src/main/resources/static/system/login/js/login.js
+++ src/main/resources/static/system/login/js/login.js
@@ -2,23 +2,45 @@
 $(document).ready(function(){
 
 
-$("#loginbtn").click(function(){
-    $("#loginbtn").button('loading').delay(1000).queue(function() {
-    });
+/*$("#login").click(function(){
+    // $("#loginbtn").button('loading').delay(1000).queue(function() {
+    // });
+    var AccountName = $("#AccountName").val();
+    var Password = $("#Password").val();
     $.ajax({
-        type: "Post",
+        type: "post",
         contentType: "application/json;charset=UTF-8",
         url: '/sys/login',
         dataType:"json",
-        data: $('#formtj').serializeJson(),
+        data:{"AccountName":AccountName,"Password":Password} ,
         success: function (data) {
-            $("#yzmimg").click();
-
-
+            toastr_success(JSON.stringify(data))
             $("#loginbtn" ).button('reset');
         }
     });
-});
+});*/
 
+    $("#login").click(function () {
+        // $("#contractCreationTemplateEditFrom").data('bootstrapValidator').validate();
+        // if ($("#contractCreationTemplateEditFrom").data('bootstrapValidator').isValid()) {
+        new AjaxRequest({
+            type: "post",
+            url: "/sys/login",
+            param: $('#loginFrom').serializeJson(),
+            tableparam: {currentpagecount: 1},
+            callBack:function (data) {
+                if (data==true){
+                    window.location.replace("/static/main.html");
+                    toastr_warning("登陆成功");
+
+                }else {
+                    window.location.replace("/static/login.html");
+                    toastr_warning("登陆失败！")
+
+                }
+            }
+        });
+        // }
+    })
 
 })
