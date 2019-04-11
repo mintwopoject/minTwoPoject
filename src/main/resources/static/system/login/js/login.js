9b4189924d4c89377380305813851c4c53f1d639
袁帅 <weiqinglin>
2019-04-11 15:22:26 +0800

    123

--- src/main/resources/static/system/login/js/login.js
+++ src/main/resources/static/system/login/js/login.js
@@ -1,25 +1,5 @@
 
 $(document).ready(function(){
-
-
-/*$("#login").click(function(){
-    // $("#loginbtn").button('loading').delay(1000).queue(function() {
-    // });
-    var AccountName = $("#AccountName").val();
-    var Password = $("#Password").val();
-    $.ajax({
-        type: "post",
-        contentType: "application/json;charset=UTF-8",
-        url: '/sys/login',
-        dataType:"json",
-        data:{"AccountName":AccountName,"Password":Password} ,
-        success: function (data) {
-            toastr_success(JSON.stringify(data))
-            $("#loginbtn" ).button('reset');
-        }
-    });
-});*/
-
     $("#login").click(function () {
         // $("#contractCreationTemplateEditFrom").data('bootstrapValidator').validate();
         // if ($("#contractCreationTemplateEditFrom").data('bootstrapValidator').isValid()) {
@@ -30,17 +10,25 @@ $(document).ready(function(){
             tableparam: {currentpagecount: 1},
             callBack:function (data) {
                 if (data==true){
-                    window.location.replace("/static/main.html");
-                    toastr_warning("登陆成功");
+                    isSuccess(data)
+                    setTimeout(function(){window.location.replace("/static/main.html");}, 1000);
 
                 }else {
-                    window.location.replace("/static/login.html");
-                    toastr_warning("登陆失败！")
-
+                    isSuccess(data)
+                    // setTimeout(function(){window.location.replace("/static/login.html");}, 1000);
                 }
             }
         });
         // }
     })
+
+
+    function isSuccess(data) {
+        if (data==true){
+            toastr_success("登陆成功");
+        }else {
+            toastr_error("用户名或密码不对，请重新登陆！")
+        }
+    }
 
 })
